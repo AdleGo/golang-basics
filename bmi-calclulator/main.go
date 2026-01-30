296289511d38a614dd75015ddb4d6860584a1655
AdleGo <askaradilet41@gmail.com>
2026-01-30 20:51:55 +0500

    separated main function into sub functions

--- bmi-calclulator/main.go
+++ bmi-calclulator/main.go
@@ -11,12 +11,28 @@ func main() {
 
 	fmt.Println("__Body Index Mass Calculator__")
 
+	userHeight, userWeight = getUserInput()
+	BMI := calculateBMI(userWeight, userHeight, BMIPower)
+	outputResult(BMI)
+}
+
+func outputResult(BMI float64) {
+	result := fmt.Sprintf("BMI: %.0f", BMI)
+	fmt.Print(result)
+}
+
+func calculateBMI(weight, height, power float64) float64 {
+	return weight / math.Pow(height/100, power)
+}
+
+func getUserInput() (float64, float64) {
+	var height, weight float64
+
 	fmt.Print("Enter user height: ")
-	fmt.Scan(&userHeight)
+	fmt.Scan(&height)
 
 	fmt.Print("Enter user weight: ")
-	fmt.Scan(&userWeight)
+	fmt.Scan(&weight)
 
-	BMI := userWeight / math.Pow(userHeight, BMIPower)
-	fmt.Printf("BMI: %v", BMI)
+	return height, weight
 }
