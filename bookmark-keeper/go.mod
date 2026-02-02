f47fbf9e36d5c0b27e59f29f409f542a8a6a5ed7
AdleGo <askaradilet41@gmail.com>
2026-02-02 16:59:00 +0500

    added bookmark keeper project

---
+++ bookmark-keeper/go.mod
@@ -0,0 +1,3 @@
+module bookmark-keeper
+
+go 1.25.6
---
+++ bookmark-keeper/main.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func main() {
+	bookmarks := map[string]string{}
+programLoop:
+	for {
+		displayMenu()
+		choice := getResult()
+
+		switch choice {
+		case 1:
+			viewBookmarks(bookmarks)
+		case 2:
+			addBookmark(bookmarks)
+		case 3:
+			deleteBookmark(bookmarks)
+		case 4:
+			break programLoop
+		}
+	}
+
+}
+
+func displayMenu() {
+	fmt.Println("- 1. View Bookmarks")
+	fmt.Println("- 2. Add Bookmark")
+	fmt.Println("- 3. Delete Bookmark")
+	fmt.Println("- 4. Exit")
+	fmt.Print("Enter choice: ")
+}
+
+func getResult() int {
+	var menuChoice int
+	fmt.Scan(&menuChoice)
+	return menuChoice
+}
+
+func viewBookmarks(bookmarks map[string]string) {
+	for k, v := range bookmarks {
+		fmt.Println(k, ":", v)
+	}
+}
+
+func addBookmark(bookmarks map[string]string) map[string]string {
+	var name, address string
+	fmt.Print("Enter name: ")
+	fmt.Scan(&name)
+	fmt.Print("Enter address: ")
+	fmt.Scan(&address)
+
+	bookmarks[name] = address
+	return bookmarks
+}
+
+func deleteBookmark(bookmarks map[string]string) map[string]string {
+	var name string
+	fmt.Print("Enter name: ")
+	fmt.Scan(&name)
+
+	delete(bookmarks, name)
+	return bookmarks
+}
